3d7824fd31f7264d3a3ae1144e00fd1f12307ed1
dimatolsti4 <44486003+dimatolsti4@users.noreply.github.com>
2018-11-15 19:40:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,68 +12,68 @@ for month in range(12):
 print(year_income)
 category = int(input())
 if category == 1:
-    if year_income<9075:
+    if year_income < 9075:
         tax = year_income*0.1
         print(tax)
-    if (year_income > 9075) and (year_income<36900):
-        tax = 90750.1+(year_income-9075)*0.15
+    if (year_income > 9075) and (year_income < 36900):
+        tax = 9075*0.1 + (year_income - 9075)*0.15
         print(tax)
-    if (year_income > 36901) and (year_income<89350):
-        tax = 90750.1+278240.15+(year_income-36901)*0.25
+    if (year_income > 36901) and (year_income < 89350):
+        tax = 9075*0.1 + 27824*0.15 + (year_income - 36901)*0.25
         print(tax)
-    if (year_income > 89351) and (year_income<186350):
-        tax = 90750.1+278240.15+524490.28+(year_income-89351)*0.28
+    if (year_income > 89351) and (year_income < 186350):
+        tax = 9075*0.1 + 27824*0.15 + 52449*0.28 + (year_income - 89351)*0.28
         print(tax)
-    if (year_income > 186351) and (year_income<405100):
-        tax = 90750.1+278240.15+524490.25+969990.28+(year_income-186351)*0.33
+    if (year_income > 186351) and (year_income < 405100):
+        tax = 9075*0.1 + 27824*0.15 + 52449*0.25 + 96999*0.28 + (year_income - 186351)*0.33
         print(tax)
-    if (year_income > 405101) and (year_income<406750):
-        tax = 90750.1+278240.15+524490.25+969990.28+2187490.33+(year_income-405101)*0.35
+    if (year_income > 405101) and (year_income < 406750):
+        tax = 9075*0.1 + 27824*0.15 + 52449*0.25 + 96999*0.28 + 218749*0.33 + (year_income - 405101)*0.35
         print(tax)
     if year_income > 406751:
-        tax = 90750.1 + 278240.15 + 524490.25 + 969990.28 + 2187490.33 + 16490.35 + (year_income-406751)*0.396
+        tax = 9075*0.1 + 27824*0.15 + 52449*0.25 + 96999*0.28 + 218749*0.33 + 1649*0.35 + (year_income - 406751)*0.396
         print(tax)
 if category == 2:
     if year_income < 18150:
         tax = year_income*0.1
         print(tax)
     if (18151 < year_income) and (year_income < 73800):
-        tax = 181510.1 + (year_income - 18151)*0.15
+        tax = 1815*10.1 + (year_income - 18151)*0.15
         print(tax)
     if (year_income > 73801) and (year_income < 148850):
-        tax = 181510.1 + 556490.15 + (year_income - 73801)*0.25
+        tax = 18151*0.1 + 55649*0.15 + (year_income - 73801)*0.25
         print(tax)
     if (year_income > 148851) and (year_income < 226850):
-        tax = 181510.1 + 556490.15 + 750490.25 + (year_income - 148851)*0.28
+        tax = 18151*0.1 + 55649*0.15 + 75049*0.25 + (year_income - 148851)*0.28
         print(tax)
     if (year_income > 226851) and (year_income < 405100):
-        tax = 181510.1 + 556490.15 + 750490.25 + 779990.28 + (year_income - 226851)*0.33
+        tax = 18151*0.1 + 55649*0.15 + 75049*0.25 + 77999*0.28 + (year_income - 226851)*0.33
         print(tax)
     if (year_income > 405101) and (year_income < 457600):
-        tax = 181510.1 + 556490.15 + 750490.25 + 779990.28 + 1782490.33 + (year_income - 405101)*0.35
+        tax = 18151*0.1 + 55649*0.15 + 75049*0.25 + 77999*0.28 + 178249*0.33 + (year_income - 405101)*0.35
         print(tax)
     if year_income > 457601:
-        tax = 181510.1 + 556490.15 + 750490.25 + 779990.28 + 1782490.33 + (year_income - 457601)*0.396
+        tax = 18151*0.1 + 55649*0.15 + 75049*0.25 + 77999*0.28 + 178249*0.33 + (year_income - 457601)*0.396
         print(tax)
-if category==3:
-    if year_income<12950:
-        tax=year_income*0.1
+if category == 3:
+    if year_income < 12950:
+        tax = year_income*0.1
         print(tax)
     if (year_income > 12950) and (year_income < 49400):
-        tax = 129500.1+(year_income-12950)*0.15
+        tax = 12950*0.1 + (year_income-12950)*0.15
         print(tax)
     if (year_income > 49401) and (127550 > year_income):
-        tax = 129500.1 + 364490.15 + (year_income-49401)*0.25
+        tax = 12950*0.1 + 36449*0.15 + (year_income - 49401)*0.25
         print(tax)
     if (year_income > 127551) and (year_income < 206600):
-        tax = 129500.1 + 364490.15 + 781490.25 + (year_income - 127551)*0.28
+        tax = 12950*0.1 + 36449*0.15 + 78149*0.25 + (year_income - 127551)*0.28
         print(tax)
     if (year_income > 206601) and (year_income < 405100):
-        tax = 129500.1 + 364490.15 + 781490.25 + 790490.28 + (year_income - 206601)*0.33
+        tax = 12950*0.1 + 36449*0.15 + 78149*0.25 + 79049*0.28 + (year_income - 206601)*0.33
         print(tax)
     if (year_income > 405101) and (year_income < 432200):
-        tax = 129500.1 + 364490.15 + 781490.25 + 790490.28 + 1984990.33 + (year_income-405101)*0.35
+        tax = 12950*0.1 + 36449*0.15 + 78149*0.25 + 79049*0.28 + 198499*0.33 + (year_income - 405101)*0.35
         print(tax)
     if year_income > 432201:
-        tax = 129500.1 + 364490.15 + 781490.25 + 790490.28 + 1984990.33 + 270990.35 + (year_income-432201)*0.396
+        tax = 12950*0.1 + 36449*0.15 + 78149*0.25 + 79049*0.28 + 198499*0.33 + 27099*0.35 + (year_income - 432201)*0.396
         print(tax)
