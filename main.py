cb424dc2c7a3d68396f054b659d07a1ef292d6d5
tsl1me <44076883+tsl1me@users.noreply.github.com>
2018-11-15 19:48:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ Timofeev A (60%), Ignatov G(40%), Rodionov D(20%)
 tax: int = 0
 year_income: int = 0
 
+print(local.slovo2)    
+    
 for month in range(12):
     month_income: int = int(input())
     year_income += month_income
 print(year_income)
-category = int(input())
+category = int(input(local.slovo))
 if category == 1:
     if year_income < 9075:
         tax = year_income*0.1
