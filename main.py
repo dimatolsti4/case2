082ddd7d97374e2f99c0ea8e955c102ed8093b43
tsl1me <44076883+tsl1me@users.noreply.github.com>
2018-11-16 09:49:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,30 +17,30 @@ if category == 1:  #подсчет для 1 категории
     elif year_income <= 36900:
         tax = 9075*0.1 + (year_income - 9075)*0.15
     elif year_income <= 89350:
-        tax = 9075*0.1 + 27824*0.15 + (year_income - 36901)*0.25
+        tax = 5081.1 + (year_income - 36901)*0.25
     elif year_income <= 186350:
-        tax = 9075*0.1 + 27824*0.15 + 52449*0.28 + (year_income - 89351)*0.28
+        tax = 19766.82 + (year_income - 89351)*0.28
     elif year_income <= 405100:
-        tax = 9075*0.1 + 27824*0.15 + 52449*0.25 + 96999*0.28 + (year_income - 186351)*0.33
+        tax = 46926.54 + (year_income - 186351)*0.33
     elif year_income <= 406750:
-        tax = 9075*0.1 + 27824*0.15 + 52449*0.25 + 96999*0.28 + 218749*0.33 + (year_income - 405101)*0.35
+        tax = 119113.71+ (year_income - 405101)*0.35
     else:
-        tax = 9075*0.1 + 27824*0.15 + 52449*0.25 + 96999*0.28 + 218749*0.33 + 1649*0.35 + (year_income - 406751)*0.396
+        tax = 119690.86 + (year_income - 406751)*0.396
 
 if category == 2:  #подсчет для 2 категории
     if year_income < 18150:
         tax = year_income*0.1
     elif year_income <= 73800:
-        tax = 1815*10.1 + (year_income - 18151)*0.15
+        tax = 1815 + (year_income - 18151)*0.15
     elif year_income <= 148850:
-        tax = 18151*0.1 + 55649*0.15 + (year_income - 73801)*0.25
+        tax = 10162.35 + (year_income - 73801)*0.25
     elif year_income <= 226850:
-        tax = 18151*0.1 + 55649*0.15 + 75049*0.25 + (year_income - 148851)*0.28
+        tax = 28924.6+ (year_income - 148851)*0.28
     elif year_income <= 405100:
-        tax = 18151*0.1 + 55649*0.15 + 75049*0.25 + 77999*0.28 + (year_income - 226851)*0.33
+        tax = 50764.32 + (year_income - 226851)*0.33
     elif year_income <= 457600:
-        tax = 18151*0.1 + 55649*0.15 + 75049*0.25 + 77999*0.28
-        tax = tax + 178249*0.33 + (year_income - 405101)*0.35
+        tax = 109586.49+(year_income - 405101)*0.35
+        
     else:
         tax = 18151*0.1 + 55649*0.15 + 75049*0.25 + 77999*0.28
         tax = tax + 178249*0.33 + 52499*0.35 + (year_income - 457601)*0.396
@@ -48,16 +48,19 @@ if category == 3:  #подсчет для 3 категории
     if year_income <=12950:
         tax = year_income*0.1
     elif year_income <= 49400:
-        tax = 1295 + (year_income-12950)*0.15
+        tax = 12950*0.1 + (year_income-12950)*0.15
     elif 127550 >= year_income:
-        tax = 6762.35 + (year_income - 49401)*0.25
+        tax = 12950*0.1 + 36449*0.15 + (year_income - 49401)*0.25
     elif year_income <= 206600:
-        tax = 26299.6 + (year_income - 127551)*0.28
+        tax = 12950*0.1 + 36449*0.15 + 78149*0.25 + (year_income - 127551)*0.28
     elif year_income <= 405100:
-        tax = 48433.32 + (year_income - 206601)*0.33
+        tax = 12950*0.1 + 36449*0.15 + 78149*0.25
+        tax = tax + 79049*0.28 + (year_income - 206601)*0.33
     elif year_income <= 432200:
-        tax = 113937.99 + (year_income - 405101)*0.35
+        tax = 12950*0.1 + 36449*0.15 + 78149*0.25 + 79049*0.28
+        tax = tax + 198499*0.33 + (year_income - 405101)*0.35
     else:
-        tax = 123422.64 + (year_income - 432201)*0.396
+        tax = 12950*0.1 + 36449*0.15 + 78149*0.25 + 79049*0.28
+        tax = tax + 198499*0.33 + 27099*0.35 + (year_income - 432201)*0.396
 print(round(tax))  #вывод  налога
 end = int(input())
