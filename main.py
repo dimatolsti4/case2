8fb0d474b8d57a17c4a6b2beb6d0c1afe3484de4
ADOLF HITLER <44486003+dimatolsti4@users.noreply.github.com>
2018-11-16 09:49:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -48,17 +48,15 @@ if category == 3:  #подсчет для 3 категории
     if year_income <=12950:
         tax = year_income*0.1
     elif year_income <= 49400:
-        tax = 12950*0.1 + (year_income-12950)*0.15
+        tax = 1295 + (year_income-12950)*0.15
     elif 127550 >= year_income:
-        tax = 12950*0.1 + 36449*0.15 + (year_income - 49401)*0.25
+        tax = 6762.35 + (year_income - 49401)*0.25
     elif year_income <= 206600:
-        tax = 12950*0.1 + 36449*0.15 + 78149*0.25 + (year_income - 127551)*0.28
+        tax = 26299.6 + (year_income - 127551)*0.28
     elif year_income <= 405100:
-        tax = 12950*0.1 + 36449*0.15 + 78149*0.25
-        tax = tax + 79049*0.28 + (year_income - 206601)*0.33
+        tax = 48433.32 + (year_income - 206601)*0.33
     elif year_income <= 432200:
-        tax = 12950*0.1 + 36449*0.15 + 78149*0.25 + 79049*0.28
-        tax = tax + 198499*0.33 + (year_income - 405101)*0.35
+        tax = 113937.99 + (year_income - 405101)*0.35
     else:
         tax = 123422.64 + (year_income - 432201)*0.396
 print(round(tax))  #вывод  налога
